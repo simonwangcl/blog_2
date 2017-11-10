463a70506dc7f37f3dd7fdbfe63409f2f07f4f73
simon <249348306@qq.com>
2017-11-10 16:50:44 +0800

    restore logo

--- resources/views/web/layout/logo.blade.php
+++ resources/views/web/layout/logo.blade.php
@@ -3,7 +3,6 @@
         <h1 class="text-center">Simon<small>Blog</small></h1>
         <p class="text-center">
             <small class="slogan">There are no regrets in life, just lessons.</small>
-            <span> ---- Jennifer Aniston</span>
         </p>
         {{--<p class="text-center">--}}
         {{--<small><a href="http://meepops.lanecn.com/">PHP Socket服务</a>  |  <a href="http://lanewechat.lanecn.com/">PHP微信开发框架</a>  |  <a href="http://www.lanecn.com/article/main/aid-11">开源博客</a></small>--}}
